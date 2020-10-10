13e18b6aab434000d498986fcd16dd7644544c3b
Alfred Young <alfred@codenaturally.com>
2020-10-09 22:00:35 -0700

    app added

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,5 @@ module.exports = {
   resolver: '@nrwl/jest/plugins/resolver',
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageReporters: ['html'],
-  projects: '<rootDir>/libs/effect-ts-cycle',
+  projects: '<rootDir>/apps/admin-dashboard',
 };
